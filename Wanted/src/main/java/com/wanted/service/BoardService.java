e25ec3f011b76e1549b89538a74480e9e50781b1
rhdmswl <eunji9909@naver.com>
2023-10-19 00:23:57 +0900

    [Feat]비즈니스 계층 추가완

---
+++ Wanted/src/main/java/com/wanted/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.wanted.service;
+
+import java.util.List;
+
+import com.wanted.domain.BoardVO;
+
+public interface BoardService {
+	public void register(BoardVO board);
+	
+	public BoardVO get(Long bno);
+	
+	public boolean modify(BoardVO board);
+	
+	public boolean remove(Long bno);
+	
+	public List<BoardVO> getList();
+
+}
